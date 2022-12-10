1c86ab807b4ef9c400d9b00bfd5c7753ad072f74
Lakshmi Chidambaranathan <lakshmi.cg@zohocorp.com>
2022-12-10 20:09:23 +0530

    check access to push

--- ZDM_iOS_SDKs/ViewController.swift
+++ ZDM_iOS_SDKs/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        //Check commit/Push
+        //Check commit/Push check check
     }
 
 
